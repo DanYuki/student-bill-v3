a8d62622f0b511b19f891dcd64835d859fca931b
Dan Yuki <daniyusuf.muhammad@gmail.com>
2023-05-08 11:55:31 +0700

    Update route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// For static pages I consider using this 
+// Route::get('/', function () {
+//     return 'welcome';
+// });
+
+// But, since index probably will contain some data from database, I'll probably use this one
+Route::get('/', [PagesController::class, 'index']);
